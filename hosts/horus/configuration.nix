75d24ad5c3e9e32bc910ea601ba78432301aa91b
Fabrice <fabrice@fabricesemti.com>
2024-06-03 18:04:45 +0100

    chore: :recycle: clean up config

--- hosts/horus/configuration.nix
+++ hosts/horus/configuration.nix
@@ -7,87 +7,31 @@
 , pkgs
 , ...
 }: {
+
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
     # outputs.nixosModules.example
-
     ../../modules/nixosModules/locale.nix
     ../../modules/nixosModules/users.nix
     ../../modules/nixosModules/packages.nix
-
     # Import host's bootloader config
     ../../modules/nixosModules/bootloader.nix
-
     # Host specific configs
     ../../modules/nixosModules/audio.nix
     ../../modules/nixosModules/xserver.nix
     ../../modules/nixosModules/hyprland.nix
-
     ../../modules/nixosModules/nixvim/nixvim.nix
-
-
+    ../../modules/nixosModules/nixConfig.nix
     # Or modules from other flakes (such as nixos-hardware):
     # inputs.hardware.nixosModules.common-cpu-amd
     # inputs.hardware.nixosModules.common-ssd
-
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
-
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
     ./networking.nix
-
-
-
   ];
-
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-    };
-  };
-
-  nix =
-    let
-      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-    in
-    {
-      settings = {
-        # Enable flakes and new 'nix' command
-        experimental-features = "nix-command flakes";
-        # Opinionated: disable global registry
-        flake-registry = "";
-        # Workaround for https://github.com/NixOS/nix/issues/9574
-        nix-path = config.nix.nixPath;
-      };
-      # Opinionated: disable channels
-      channel.enable = false;
-
-      # Opinionated: make flake registry and nix path match flake inputs
-      registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
-      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-    };
-
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
@@ -101,10 +45,9 @@
       PasswordAuthentication = false;
     };
   };
-
+  
   # Install firefox.
   programs.firefox.enable = true;
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
 }
